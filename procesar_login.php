dff0676a2c64cec78304c956def182780b75d068
ysf <145535591+youkaa04@users.noreply.github.com>
2026-04-27 13:52:17 +0200

    sss

--- procesar_login.php
+++ procesar_login.php
@@ -17,7 +17,7 @@ $nombre = $_POST['nombre'];
 $password = $_POST['password'];
 
 // Buscar usuario
-$stmt = $pdo->prepare("SELECT * FROM usuarios WHERE nombre = ?");
+$stmt = $pdo->prepare("SELECT * FROM usuario WHERE nombre = ?");
 $stmt->execute([$nombre]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -26,11 +26,10 @@ if (!$user) {
 }
 
 // Verificar contraseña
-if (password_verify($password, $user['password'])) {
+if (password_verify($password, $user['pass'])) {
     $_SESSION['usuario'] = $user['nombre'];
-    echo "Login correcto";
-    // Aquí puedes redirigir al panel admin
-    // header("Location: admin.php");
+    header("Location: admin.php");
+    exit;
 } else {
     echo "Contraseña incorrecta";
 }
