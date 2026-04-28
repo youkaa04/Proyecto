f4c7ceae11ad3909d34075aed158a795e9449ae0
youssef <youssef@NTC24051503.upvnet.upv.es>
2026-04-28 13:03:39 +0200

    Last one for today

--- Iniciar_sesion.php
+++ Iniciar_sesion.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" href="Style/style.css?v=2">
     <link rel="stylesheet" href="Style/style2.css">
 </head>
 <body>
@@ -26,6 +27,60 @@
 
     <p>¿No tienes cuenta? <a href="Registro.php">Regístrate</a></p>
 </div>
+<footer class="footer">
+    <div class="footer-inner">
+
+        <div class="footer-left">
+            <h3>Contacto</h3>
+            <p>Soporte: <a href="mailto:suporte.joystickdrawer@gmail.com">suporte.joystickdrawer@gmail.com</a></p>
+        </div>
+
+        <div class="footer-right">
+            <h3>Síguenos</h3>
+            <div class="social-icons">
+
+                <a href="https://www.instagram.com/joystickdrawer/" target="_blank">
+                    <img src="Imagenes/icons/instagram.png" alt="Instagram">
+                </a>
+
+                <a href="" target="_blank">
+                    <img src="Imagenes/icons/tiktok.png" alt="TikTok">
+                </a>
+
+                <a href="" target="_blank">
+                    <img src="Imagenes/icons/linkedin.png" alt="LinkedIn">
+                </a>
+
+                <a href="" target="_blank">
+                    <img src="Imagenes/icons/twitch.png" alt="Twitch">
+                </a>
+
+                <a href="" target="_blank">
+                    <img src="Imagenes/icons/youtube.png" alt="YouTube">
+                </a>
+
+                <a href="" target="_blank">
+                    <img src="Imagenes/icons/facebook.png" alt="Facebook">
+                </a>
+
+                <a href="" target="_blank">
+                    <img src="Imagenes/icons/x.png" alt="X">
+                </a>
+
+            </div>
+        </div>
+
+    </div>
+    <div class="pie_container">
+        <strong class="pie_strong">
+            Plataforma feta amb estima per la Comunitat Valenciana.<br>
+            Un punt de trobada per a tots els valencians i valencianes.
+        </strong>
+    </div>
+    <div class="footer-bottom">
+        © 2026 JOYSTICKDRAWER — Todos los derechos reservados
+    </div>
+</footer>
 
 </body>
 </html>
